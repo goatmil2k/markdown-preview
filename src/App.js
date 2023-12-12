28b2206ea87621da0b0a9dc7e21185f17efc6847
Krishna <krishnakaflay200@gmail.com>
2023-12-12 08:55:20 +0630

    added dot env

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Toolbar from './components/Toolbar';
 import Previewer from './components/Markdown';
 
